7151f9dba8b53ce1349fc17016611b681b31f1d1
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2022-08-10 11:03:59 +0700

    change from 3D array to 2D vector

--- Data_Structure/ex00e2_Min_Max.cpp
+++ Data_Structure/ex00e2_Min_Max.cpp
@@ -6,12 +6,12 @@
 */
 #include<bits/stdc++.h>
 using namespace std;
-int a[110][110];
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
     int n,m,q;
     cin >> n >> m >> q;
+    vector<vector<int>> a(n+1, vector<int>(m+1));
     for(int i=1;i<=n;i++)
         for(int j=1;j<=m;j++)
             cin >> a[i][j];
