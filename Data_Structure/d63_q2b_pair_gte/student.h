0a6b189c50cccfbe37d069d893aa248e88517446
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2022-09-14 09:58:33 +0700

    Week 2 of September

--- Data_Structure/d63_q2b_pair_gte/student.h
+++ Data_Structure/d63_q2b_pair_gte/student.h
@@ -3,8 +3,10 @@
 
 template <typename T1,typename T2>
 bool CP::pair<T1,T2>::operator>=(const pair<T1,T2>& other) const {
-  //write your code here
-  return false;
+	//write your code here
+	if(this->first > other.first)	return true;
+	if(this->first == other.first &&   this->second >= other.second)	return true;
+	return false;
 }
 
 #endif
