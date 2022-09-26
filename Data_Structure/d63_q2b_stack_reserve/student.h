614cac02d7e6f391c4b896532ba5ace2ff5bd64f
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2022-09-26 08:20:05 +0700

    update stack reserve

--- Data_Structure/d63_q2b_stack_reserve/student.h
+++ Data_Structure/d63_q2b_stack_reserve/student.h
@@ -4,6 +4,10 @@
 template <typename T>
 int CP::stack<T>::compare_reserve(const CP::stack<T> &other) const {
     //write your code here
+	int remaining = mCap - mSize;
+	int otherRemaining = other.mCap - other.mSize;
+	int dif = remaining - otherRemaining;
+	return ((dif > 0)?1:((dif < 0)?-1:0));
 }
 
 #endif
