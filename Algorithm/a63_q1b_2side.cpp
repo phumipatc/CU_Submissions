5d48c5a9d7e0bd3422cf7fc2338b7053c482c7b5
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2023-02-26 23:32:14 +0700

    update

--- Algorithm/a63_q1b_2side.cpp
+++ Algorithm/a63_q1b_2side.cpp
@@ -1,8 +1,8 @@
 /*
-	Task	: a63_q1a_buffet
+	Task	: a63_q1b_2side
 	Author	: Phumipat C. [MAGCARI]
 	Language: C++
-	Created	: 26 February 2023 [23:05]
+	Created	: 26 February 2023 [23:19]
 */
 #include<bits/stdc++.h>
 #define rep(i, a, b) for(int i = a; i <= (b); ++i)
@@ -22,19 +22,41 @@ using PLL = pair<long long ,long long >;
 const int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
 const int dir8[2][8] = {{-1,-1,-1,0,1,1,1,0},{-1,0,1,1,-1,0,1,-1}};
 const int N = 2e5+10;
-int a[N];
+int a[N][2],dp[N][2],dp2[N][2][55];
 int main(){
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
-	int n,k,m;
-	cin >> n >> k >> m;
+	int n,w,k;
+	cin >> n >> w >> k;
 	rep(i,1,n)
-		cin >> a[i],a[i]+=a[i-1] - m;
-	while(k--){
-		int p,w;
-		cin >> p >> w;
-		int idx = lower_bound(a+p,a+n+1,a[p-1]+w)-a;
-		cout << idx << '\n';
+		cin >> a[i][0];
+	rep(i,1,n)
+		cin >> a[i][1];
+	if(k == n){
+		rep(i,1,n){
+			// left at i
+			dp[i][0] = dp[max(0,i-w-1)][1] + a[i][0];
+			dp[i][0] = max(dp[i-1][0],dp[i][0]);
+			// right at i
+			dp[i][1] = dp[max(0,i-w-1)][0] + a[i][1];
+			dp[i][1] = max(dp[i-1][1],dp[i][1]);
+		}
+		cout << max(dp[n][0],dp[n][1]) << '\n';
+	}else{
+		int mx = 0;
+		rep(i,1,n){
+			rep(j,1,k){
+				// left at i
+				dp2[i][0][j] = dp2[max(0,i-w-1)][1][j-1] + a[i][0];
+				dp2[i][0][j] = max(dp2[i-1][0][j],dp2[i][0][j]);
+				mx = max(mx,dp2[i][0][j]);
+				// right at i
+				dp2[i][1][j] = dp2[max(0,i-w-1)][0][j-1] + a[i][1];
+				dp2[i][1][j] = max(dp2[i-1][1][j],dp2[i][1][j]);
+				mx = max(mx,dp2[i][1][j]);
+			}
+		}
+		cout << mx << '\n';
 	}
 	return 0;
 }
